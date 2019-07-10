817807b3c55e0b6f6d42eee34fd8ec3e5eaab9ce
Artem Sidorkin <artem.sidorkin@digitalarrowtech.com>
2019-07-10 17:37:16 +0300

    Fixed according codereview

--- aws-fargate-terraform/sample.sql
+++
@@ -1,4 +0,0 @@
-Executing this should cause a message to be published to Kafka
-
-use eventuate;
-insert into message(id,destination, headers,payload) values('x5', 'mytopic', '{ "ID" : "foo"}', '{}');
