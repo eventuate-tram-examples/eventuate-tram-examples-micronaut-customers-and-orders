9def0eac6d5cab38d7817caf5fd3e7f95ce9bb31
Artem Sidorkin <artem.sidorkin@digitalarrowtech.com>
2019-08-01 14:12:49 +0300

    Updated to latest tram and common.

--- customer-backend/src/main/java/io/eventuate/examples/tram/ordersandcustomers/customers/CustomerFactory.java
+++ customer-backend/src/main/java/io/eventuate/examples/tram/ordersandcustomers/customers/CustomerFactory.java
@@ -4,8 +4,13 @@ import io.eventuate.examples.tram.ordersandcustomers.customers.service.OrderEven
 import io.eventuate.tram.events.subscriber.DomainEventDispatcher;
 import io.eventuate.tram.events.subscriber.DomainEventDispatcherFactory;
 import io.micronaut.context.annotation.Factory;
+import org.hibernate.SessionFactory;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import javax.inject.Singleton;
+import javax.persistence.EntityManagerFactory;
 
 @Factory
 public class CustomerFactory {
@@ -13,5 +18,10 @@ public class CustomerFactory {
   public DomainEventDispatcher domainEventDispatcher(OrderEventConsumer orderEventConsumer,
                                                      DomainEventDispatcherFactory domainEventDispatcherFactory) {
     return domainEventDispatcherFactory.make("orderServiceEvents", orderEventConsumer.domainEventHandlers());
+  }
+
+  @Singleton
+  public TransactionTemplate transactionTemplate(SessionFactory sessionFactory) {
+    return new TransactionTemplate(new HibernateTransactionManager(sessionFactory));
   }
 }
--- order-backend/src/main/java/io/eventuate/examples/tram/ordersandcustomers/orders/OrderFactory.java
+++ order-backend/src/main/java/io/eventuate/examples/tram/ordersandcustomers/orders/OrderFactory.java
@@ -4,13 +4,23 @@ import io.eventuate.examples.tram.ordersandcustomers.orders.service.CustomerEven
 import io.eventuate.tram.events.subscriber.DomainEventDispatcher;
 import io.eventuate.tram.events.subscriber.DomainEventDispatcherFactory;
 import io.micronaut.context.annotation.Factory;
+import org.hibernate.SessionFactory;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import javax.inject.Singleton;
+import javax.persistence.EntityManagerFactory;
 
 @Factory
 public class OrderFactory {
   @Singleton
   public DomainEventDispatcher domainEventDispatcher(CustomerEventConsumer customerEventConsumer, DomainEventDispatcherFactory domainEventDispatcherFactory) {
     return domainEventDispatcherFactory.make("consumerServiceEvents", customerEventConsumer.domainEventHandlers());
+  }
+
+  @Singleton
+  public TransactionTemplate transactionTemplate(SessionFactory sessionFactory) {
+    return new TransactionTemplate(new HibernateTransactionManager(sessionFactory));
   }
 }
