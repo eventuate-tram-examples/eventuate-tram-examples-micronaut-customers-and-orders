e3745ee9e0687709740503269cbd2fa5354e0121
Artem Sidorkin <artem.sidorkin@digitalarrowtech.com>
2019-10-03 15:16:16 +0300

    Fixed container start up. Added swagger.

--- order-history-service/src/main/java/io/eventuate/examples/tram/ordersandcustomers/orderhistoryservice/OrderHistoryServiceMain.java
+++ order-history-service/src/main/java/io/eventuate/examples/tram/ordersandcustomers/orderhistoryservice/OrderHistoryServiceMain.java
@@ -1,7 +1,9 @@
 package io.eventuate.examples.tram.ordersandcustomers.orderhistoryservice;
 
 import io.micronaut.runtime.Micronaut;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 
+@OpenAPIDefinition
 public class OrderHistoryServiceMain {
 
   public static void main(String[] args) {
